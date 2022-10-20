9f439f39cb986d7d32e63c936d831c7873ff51fa
Craig <craig.skicko@gmail.com>
2022-10-20 19:23:14 +1100

    Updated typeDefs to include queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,22 @@
 // Require graph ql
 const { gql } = require('apollo-server-express');
 
+// _____________________________________________________
+// 1. Set up schemas for user, sessions and skills,
+// 2. Set up queries to 
+//      a. Get a learner by id
+//      b. Get a coach by id
+//      c. Get users by attained skills
+//      d. Get all skills
+//      e. Get all sessions that a user is involved in
+// 3. Set up mutations to
+//      a. Create a user
+//      b. Update a user by id
+//      c. Add a session
+//      d. Update a session by id
+//      e. Delete a session by id
+// _____________________________________________________
+
 const typeDefs = gql`
     type User {
         _id: ID!
@@ -26,11 +42,27 @@ const typeDefs = gql`
         date: String!
         time: String!
         confirmed: Boolean!
-        message: Text!
+        message: String!
         skills: [Skills]!
     }
 
     type Skills {
+        _id: ID
         title: String
+    }
+
+    type Query {
+        learner(userId: ID!): User
+        coach(userId: ID!): User
+        usersBySkill(title: String!): [Skill]
+        userSessions(userId: ID!): User
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): User
+        updateUser(displayName: String, learner: Boolean, coach: Boolean, learnerProfile: String, coachProfile: String, jobTitle: String, desiredSkills: [Skills], attainedSkills: [Skills], sessions: [Sessions], savedCoaches: [User]): User
+        addSession(coachId: ID!, learnerId: ID!, date: String!, time: String!, confirmed: Boolean!, message: String!, skillIds: [ID]!): Sessions
+        updateSession(sessionId: ID!, date: String!, time: String!, message: String!): Sessions
+        deleteSession: (sessionId: ID): Sessions
     }
 `
