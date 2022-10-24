b29d25cf050c2220e900eb8273d5b7d6a2c80c9d
Craig <craig.skicko@gmail.com>
2022-10-24 21:20:09 +1100

    Added front end authorisation utility function

---
+++ client/src/utils/auth.js
@@ -0,0 +1,33 @@
+import decode from 'jwt-decode';
+
+// Create the auth service class with necessary methods
+class AuthService {
+    // Get the user details
+    getUser() {
+        return decode(this.getToken());
+    };
+
+    // Check if a user is loggen in
+    loggedIn() {
+        const token = this.getToken();
+        return token ? true : false;
+    };
+
+    // Get the token from local storage
+    getToken() {
+        return localStorage.getItem('id_token');
+    };
+
+    login(idToken) {
+        localStorage.setItem('id_token', idToken);
+        // **TO DO: Update this to the profile page when it's been generated
+        window.location.assign('/');
+    };
+
+    logout() {
+        localStorage.removeItem('id_token');
+        window.location.reload();
+    };
+};
+
+export default new AuthService();
