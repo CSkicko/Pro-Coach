0c7bb2a35a4197dd16341ee1a3faf98e91a3651a
Craig <craig.skicko@gmail.com>
2022-10-28 21:42:51 +1100

    Added functionality to display saved skills on skills tab of user profile

--- client/src/components/ProfileTabs.js
+++ client/src/components/ProfileTabs.js
@@ -8,6 +8,7 @@ import Grid from '@mui/material/Grid';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Chip from '@mui/material/Chip';
 
 export default function ProfileTabs(data) {
     // Set up navigation value
@@ -20,7 +21,7 @@ export default function ProfileTabs(data) {
 //   Set up page content state
   const [pageContent, setPageContent] = React.useState(
     <>
-      <Grid item xs={4} sx={{ mt: '10%' }}>
+      <Grid item xs={4}>
         {/* Create an avatar with the first letter of the display name */}
         <Avatar sx={{ bgcolor:'#47a4d2', height:'100px', width:'100px', mx:'auto' }}>{userInfo.profile.displayName.split('').shift()}</Avatar>
         <Box sx={{ textAlign: 'center' }}>
@@ -42,18 +43,65 @@ export default function ProfileTabs(data) {
     setValue(newValue);
     // Set up switch to conditionally load page content
     switch(newValue) {
+        // If it's the second step, set the page content to the session information
         case 1:
-            return setPageContent(<h1>Sessiosn</h1>)
+            return setPageContent(<h1>Sessions</h1>)
+        // If it's the third step, set the page content to the skills information
         case 2:
-            return setPageContent(<h1>Search Coaches</h1>)
+            return setPageContent(
+              <>
+                {/* If the user doesn't have any skills saved, display a message */}
+                {userInfo.profile.skills.length < 1 ? (
+                  <>
+                    <h3>You currently don't have any saved skills.</h3>
+                    <h4>Add skills using the button below!</h4>
+                  </>
+                ) : (
+                  // If a user does have skills, display them
+                  <>
+                    <Grid item xs={12} sx={{ mb: '5%', textAlign: 'center' }}>
+                      <h2>My Skills</h2>
+                    </Grid>
+                    <Grid item xs={8} sx={{ textAlign: 'center' }}>
+                      {userInfo.profile.skills.map((skill, index) => {
+                        return <Chip label={skill.title} sx={{ m:'2%' }} />
+                      })}
+                    </Grid>
+                  </>
+                )}
+                {/* Add skills button */}
+                <Grid item xs={12} sx={{ mt: '10%', textAlign: 'center' }}>
+                    <Button variant='contained'>Add Skills</Button>
+                </Grid>
+              </>
+            )
+        // Default to display the user profile information
         default:
-            return setPageContent(<h1>{userInfo.profile.displayName}</h1>)
+            return setPageContent(
+              <>
+                <Grid item xs={4}>
+                  {/* Create an avatar with the first letter of the display name */}
+                  <Avatar sx={{ bgcolor:'#47a4d2', height:'100px', width:'100px', mx:'auto' }}>{userInfo.profile.displayName.split('').shift()}</Avatar>
+                  <Box sx={{ textAlign: 'center' }}>
+                    <h2 >{userInfo.profile.displayName}</h2>
+                    <h4>{userInfo.profile.jobTitle}</h4>
+                  </Box>
+                </Grid>
+                <Grid item xs={6} sx={{ mt: '10%' }}>
+                  <h3>Profile</h3>
+                  <p>{userInfo.profile.about}</p>
+                </Grid>
+                <Grid item xs={12} sx={{ mt: '10%', textAlign: 'center' }}>
+                  <Button variant='contained'>Edit Profile</Button>
+                </Grid>
+              </>
+            )
     };
   };
 
   return (
     <>
-        <Tabs value={value} onChange={handleChange} aria-label="Navigation Tabs">
+        <Tabs value={value} sx={{ mb: '10%' }} onChange={handleChange} aria-label="Navigation Tabs">
             <Tab label="Profile" />
             <Tab label="Sessions" />
             <Tab label="Skills" />
